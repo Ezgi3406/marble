03be8ef71602f4012403fa78eb01e23a1d8c9b69
Tugce <tugceergur191@gmail.com>
2021-07-08 23:36:32 +0300

    Header.js güncelleme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {FaInstagram,FaFacebookSquare,FaTwitter}from 'react-icons/fa'
+import {FaInstagram,FaFacebookSquare,FaTwitter,FaSignInAlt}from 'react-icons/fa'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css'
 
@@ -9,13 +9,15 @@ export default class Header extends Component {
         return (
             <div>
                 <header  >
-                
+                <ul class="social-list-left">
+                <li class="social-list__item"><a class="social-list__link" href=""><img class="img-header" src="https://cdn.goadventureturkey.com/goturkey/static/images/turkiye-dark.svg"/></a></li></ul>
                 
                 <ul class="social-list" >
-                <li class="social-list__item"><a class="social-list__link" href=""><img class="img-header" src="https://cdn.goadventureturkey.com/goturkey/static/images/turkiye-dark.svg"/></a></li>
-                  <li class="social-list__item"><a class="social-list__link" href=""><FaFacebookSquare/></a></li>
-                  <li class="social-list__item"><a class="social-list__link" href=""><FaInstagram/></a></li>
-                  <li class="social-list__item"><a class="social-list__link" href=""><FaTwitter/></a></li>
+                    <li title="Sign in" style={{margin:".25em 1.75em"}} class="social-list__item"><a class="social-list__link" href=""><FaSignInAlt/> SIGN IN</a></li>
+                    
+                    <li class="social-list__item"><a class="social-list__link" href=""><FaFacebookSquare/></a></li>
+                    <li class="social-list__item"><a class="social-list__link" href=""><FaInstagram/></a></li>
+                    <li class="social-list__item"><a class="social-list__link" href=""><FaTwitter/></a></li>
                 </ul>
                 </header>
             </div>
@@ -25,3 +27,5 @@ export default class Header extends Component {
 /*className="py-0 bg-dark"
 className="d-inline-block align-top m-1 p-0 text-right text-white"
 */ 
+
+
