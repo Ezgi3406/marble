fa76eab8f69ce904a2c9fd73ed2a6375e8566ec9
Tugce <76748512+tugce23@users.noreply.github.com>
2021-07-26 17:40:18 +0300

    login update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/index.css'
 import App from './App'
-import {AppProvider} from './components/context'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './redux/reducers';
+//import {AppProvider} from './components/context'
+
+const middleware = applyMiddleware(promiseMiddleware, ReduxThunk);
+
 ReactDOM.render(
-  <React.StrictMode>
-   <AppProvider>
-   <App />
-   </AppProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <Provider store={createStore(reducer, composeWithDevTools(middleware))}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
