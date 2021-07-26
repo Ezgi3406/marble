84553c198ef8bdce4bc70b6559ede5d9ce463b07
Tugce <76748512+tugce23@users.noreply.github.com>
2021-07-26 16:29:53 +0300

    header js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import {FaInstagram,FaFacebookSquare,FaTwitter,FaSignInAlt}from 'react-icons/fa'
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Image, Container, Row, Col } from "react-bootstrap";
 import './Header.css'
-
-
 
 export default class Header extends Component {
     render() {
