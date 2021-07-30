b69513d6fd9cc303eda4e637a8470a9318de88a9
Büşra <you@example.com>
2021-07-30 21:35:30 +0300

    Header.js updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,71 +9,11 @@ import Navbar from './Navbar/Navbar'
 export default class Header extends Component {
     render() {
         return (
-         
-          
-              
-            
-            
+        <div>
           <header  >
-          <Navbar></Navbar>
-          {/* <div style={{alignItems: 'center'}}>
-  <div className="header-language-selection">
-    <div className="header-language-selection-current">EN<span aria-label="icon-arrow-down" className="c-icon c-icon-small icon-arrow-down icon" role="img" /></div>
-    <div className="header-language-selection-dropdown">
-      <div className="header-language-selection-dropdown-list">
-        <li>English</li>
-        <li>Türkçe</li>
-        <li>русский язык</li>
-        <li>українська</li>
-        <li>العربية</li>
-        <li>Deutsch</li>
-        <li>español, castellano</li>
-        <li>français, langue française</li>
-        <li>中文 (Zhōngwén), 汉语, 漢語</li>
-        <li>فارسی</li>
-      </div>
-    </div>
-  </div>
-  <a href="https://goturkiye.com" className="header-goturkey-logo d-none-xs d-if-sm"><img src="https://cdn.goadventureturkey.com/goturkey/static/images/turkiye-dark.svg" alt /></a>
-</div>
-
-           */}
-          {/* <ul className="social-list" >
-              <li title="Sign in" className="social-list__item"><a className="social-list__link" href=""><FaSignInAlt/> SIGN IN</a></li>
-               */}
-              {/* <li class="social-list__item"><a class="social-list__link" href=""><FaFacebookSquare/></a></li>
-              <li class="social-list__item"><a class="social-list__link" href=""><FaInstagram/></a></li>
-              <li class="social-list__item"><a class="social-list__link" href=""><FaTwitter/></a></li> */}
-          {/* </ul> */}
-
-          {/*<Container>
-            <Row className="show-grid  ">
-
-              <Col xs={12} sm={12} className="icons ">
-                <div className="social-icons">
-                  <Image
-                    src="/assets/img/logo_facebook.png"
-                    className="social-icon"
-                  />
-                  <Image
-                    src="/assets/img/logo_twiter.png"
-                    className="social-icon"
-                  />
-                  <Image
-                    src="/assets/img/logo_google+.png"
-                    className="social-icon"
-                  />
-                  <Image
-                    src="/assets/img/logo_linkedin.png"
-                    className="social-icon"
-                  />
-                </div>
-              </Col>
-
-            </Row>
-          </Container>
-          */}</header>
-      </div>
+            <Navbar/>
+          </header>
+        </div>
 
   )
 }
