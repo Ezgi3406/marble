c5867180f6ff713e98a3b1b3a0171467ba3ede99
fidan574 <76748653+fidan574@users.noreply.github.com>
2021-09-06 17:58:46 +0100

    tekrar request

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,49 +1,18 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import { FaSearch } from "react-icons/fa";
+import "../pages/ege/search.css";
 
 function Search() {
   return (
-    <div >
-      <Form  >
-        <div style={{
-      display: "grid",
-      "grid-template-columns": "1fr 1fr",
-      "margin-top": "200px",
-      "margin-bottom": "300px",
-      "margin-left": "400px",
-      "margin-right": "150px",
-      
-    }}>
-          <Form.Control placeholder="Search" />
-          <button
-            type="button"
-            className="btn signin-btn"
-            style={{ margin: "0px" }}
-          >
-            <svg width="15px" height="15px">
-              <FaSearch />
-            </svg>
-          </button>
-          {/* @media screen and (max-width: 600px) {
-  .topnav .search-container {
-    float: none;
-  }
-  .topnav a, .topnav input[type=text], .topnav .search-container button {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-} */}
-        </div>
-      </Form>
-    </div>
+    <div>
+    <div id="wrap">
+    <form action="" autocomplete="on">
+    <input style={{position: "sticky"}} id="search" name="search" type="text" placeholder="What're we looking for ?"/><input id="search_submit"  type="submit"/>
+    </form>
+  </div>
+  </div>
+  
   );
 }
 export default Search;
